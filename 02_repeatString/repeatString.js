c6a248cb53a434b24f466ea00e042abe7c527a71
neil <neilmartinrobertson25@gmail.com>
2023-04-04 00:09:51 +0100

    This commit solves #02 repeatStrings

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,20 @@
-const repeatString = function() {
+const repeatString = function (str, num) {
+  //We have to repeat the string by the exact number of times the number argument is and no more
+  //We need to use a for loop for this
+  //after that is done we need to return that value
+  //The Strings will all be one string
+  // We would need to create an empty string variable to hold the new string
+  //Then we would add the entered string onto that and keep going until it reaches the number of times specified
 
+  let newWord = "";
+  if (num < 0) {
+    return "ERROR";
+  }
+
+  for (let i = 0; i < num; i++) {
+    newWord += str;
+  }
+  return newWord;
 };
 
 // Do not edit below this line
