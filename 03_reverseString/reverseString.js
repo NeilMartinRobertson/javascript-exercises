7d39fd741f1379fa67d5bcc92e0568d09426b090
neil <neilmartinrobertson25@gmail.com>
2023-04-04 00:40:11 +0100

    This commit solves #03 reverseStrings

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,21 @@
-const reverseString = function() {
+const reverseString = function (str) {
+  /*Reversing a String with Built In Methods 
+  
+    const newWord = str.split("");
+  const reversedWord = newWord.reverse();
+  const test = reversedWord.join("");
+*/
 
+  // Using a For Loop
+
+  let newString = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    newString += str[i];
+  }
+
+  return newString;
+
+  l;
 };
 
 // Do not edit below this line
