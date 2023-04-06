e190a7c5ac663184d737ebd5d3e5b69b4e00d467
neil <neilmartinrobertson25@gmail.com>
2023-04-06 01:01:44 +0100

    This commit completes #11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,8 @@
-const getTheTitles = function() {
-
-};
+const getTheTitles = function (obj) {
+    const newObj = [... obj]
+    const titles = newObj.map((element) => element.title)
+    return titles
+}
 
 // Do not edit below this line
 module.exports = getTheTitles;
