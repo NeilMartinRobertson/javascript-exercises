af136b2b0843957e4eb5400e3be3bd1e32615a94
neil <neilmartinrobertson25@gmail.com>
2023-04-06 01:01:10 +0100

    This commit completes #06

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
-	
+const add = function (num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function (num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  return arr.reduce((acc, current) => acc + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+  return arr.reduce((acc, current) => acc * current);
 };
 
-const power = function() {
-	
+const power = function (num1, num2) {
+  return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  if (num < 0) {
+    return undefined;
+  }
+  if (num === 0) {
+    return 1;
+  }
+  let factorial = num - 1;
+  while (factorial > 1) {
+    num *= factorial;
+    factorial--;
+  }
+  return num;
 };
 
 // Do not edit below this line
@@ -29,5 +40,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
